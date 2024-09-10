82849c8002c0d1b74be8800891c5a3d8e65354f1
Marcelo <marceloafiedlers@gmail.com>
2024-09-10 19:52:53 -0300

    first commit

---
+++ src/test/java/br/univille/fsoweb2024/Fsoweb2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.fsoweb2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fsoweb2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
